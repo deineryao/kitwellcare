64dc33ddc64dd38bb4c338714a418efb753e8fc7
deineryao <362526965@qq.com>
2016-03-29 22:49:07 +0800

    just one last dance

---
+++ js/public.js
@@ -0,0 +1,9 @@
+//js获取日期
+function GetDateStr(AddDayCount) { 
+	var dd = new Date(); 
+	dd.setDate(dd.getDate()+AddDayCount);//获取AddDayCount天后的日期 
+	var y = dd.getFullYear(); 
+	var m = dd.getMonth()+1;//获取当前月份的日期 
+	var d = dd.getDate(); 
+	return y+'-'+(m<10?"0"+m:m)+'-'+(d<10?"0"+d:d); 
+}
